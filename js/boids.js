4e77cd1b6d406803c8b9256534078291175dce8f
PJRyn <peregrinryan@gmail.com>
2024-02-23 14:54:40 +1100

    completed tutorial provided by codetrain

--- js/boids.js
+++ js/boids.js
@@ -1,15 +1,45 @@
 /*
+Fixes for tutorial:
+    - Alow detection through screen wrap
+    - Fix the order of processing velocity so it's simultanious
+    - label the sliders
+    - bigger screen size, maybe resizable
+    - perception arcs
+Proposed fixes from tutorial:
+    - Snapshot of all the velocities
+    - Optimization:
+        - spatial subdivsion
+        - quad tree
+        - Only do one calc for distances 
+    - interface, with other params
+        - perception radious
+            - 3 main values different perception sliders
+        - max force
+        - max speed
+    - Design
+        - make them triangles
+    - boids with diff perameters
+        - maybe colors
+    - view rule, limit its view to an arc
+        - computational beuaty of nature
+            - Keep view clear, this will result in a more realistic version
+            birds flock in triangles
+    - obsticles
+    - other forces like wind of current
+    -preditor
 What i want to add:
-    - Random vector change that is outweighed in flock
-        -Giveing a wander effect where they just vibe
+    - Give thier movement a randomising effect, gives a wandering 
+        effect when alone, might prevent striaght lines occuring
     - Button to add more to flock or remove from flock
     - Different colors which wont flock together but will reprel
-    - collision
+    - collision with other boids
     - preditor boids that dont flock but hunt boids
     - location setter so they will go to a location on mouse click
     - Sliders for each param (speed, closeness, etc.)
-    - add layers of steering diff, it slowly effects the boid and changes steering
-*/
+    - Make the avoidance become stronger based on range
+    - Solid edges
+    - 
+    */
 
 class Boid {
     constructor(){
@@ -91,8 +121,16 @@ class Boid {
         
         let alignment = this.align(boids); 
         let cohesion = this.cohesion(boids);
+        let separation = this.separation(boids);
+
+        separation.mult(separationSlider.value());
+        alignment.mult(alignSlider.value());
+        cohesion.mult(cohesionSlider.value());
+
+        this.acceleration.add(separation);
         this.acceleration.add(alignment);
         this.acceleration.add(cohesion);
+
     }
 
     update() {
@@ -108,6 +146,32 @@ class Boid {
         point(this.position.x, this.position.y)
     }
 
-
+    separation(boids) {
+        let perception = 50;
+        let steering = createVector();
+        let total = 0;
+        for (let other of boids){
+            let d = dist(
+                this.position.x,
+                this.position.y, 
+                other.position.x, 
+                other.position.y
+                );
+            if (d < perception && other != this) {
+                let diff = p5.Vector.sub(this.position, other.position);
+                diff.div(d ^ 2);
+                steering.add(diff);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            //steering.sub(this.position);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+        }
+        return steering;
+    }
 
 }
